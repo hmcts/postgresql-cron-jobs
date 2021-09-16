2d2ee9d06fcd273235ef9fdc25a2435f7ea42be2
mokainos <72137340+mokainos@users.noreply.github.com>
2021-09-16 15:35:05 +0100

    dialy jobs

---
+++ scripts/ccd-data-store-api-postgres-db-v11-prod-ccd_data_store-daily-grantsissuedyesterdayextract.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cat <<EOF
+COPY (
+SELECT
+reference AS ccd_reference,
+to_char(CAST (data ->> 'grantIssuedDate' AS DATE), 'DD/MM/YYYY') AS grant_issued_date,
+trim(data->>'deceasedForenames') AS deceasedForenames,
+trim(data->>'deceasedSurname') AS deceasedSurname,
+trim(data->>'registryLocation') AS registryLocation
+FROM case_data
+WHERE jurisdiction='PROBATE' AND (data->>'grantIssuedDate')::date = '$YESTERDAY' ORDER BY 4) to stdout with csv header;
+EOF
