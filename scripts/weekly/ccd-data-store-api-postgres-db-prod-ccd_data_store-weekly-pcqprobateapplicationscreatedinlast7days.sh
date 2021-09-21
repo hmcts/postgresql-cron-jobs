f781aafc791847d716caa20e81045900e61a81d9
mokainos <72137340+mokainos@users.noreply.github.com>
2021-09-21 14:59:26 +0100

    updated

--- scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-pcqprobateapplicationscreatedinlast7days.sh
+++ scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-pcqprobateapplicationscreatedinlast7days.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DAYSAGO=$(date -d "7 days ago" '+%Y%m%d 00:00:00')
 cat <<EOF
 COPY (
  SELECT reference, created_date, state, data->>'pcqId' AS PCQ_id, data->>'paperForm' AS paperForm, data->>'applicationType' AS applicationType, case_type_id AS type FROM case_data WHERE jurisdiction = 'PROBATE' AND data->>'pcqId' IS NOT NULL AND created_date >= '${DAYSAGO}' ORDER BY 2) TO STDOUT with csv header ;
