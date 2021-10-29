2bb408e5253c50e1805b35806f3397ea025162df
mokainos <72137340+mokainos@users.noreply.github.com>
2021-10-29 12:53:29 +0100

    debug

--- scripts/daily/rd-location-ref-api-postgres-db-prod-dbrdlocationref-daily-locationrefdatarowcount.sh
+++ scripts/daily/rd-location-ref-api-postgres-db-prod-dbrdlocationref-daily-locationrefdatarowcount.sh
@@ -2,10 +2,10 @@
 cat <<EOF
 select '' AS "locrefdata.SERVICE_TO_CCD_CASE_TYPE_ASSOC Count" ;
 select count (*) from locrefdata.SERVICE_TO_CCD_CASE_TYPE_ASSOC;
-select '' AS "locrefdata.dataload_exception_record Count" ;
-select count(*) as total_exception  from locrefdata.dataload_exception_records;
-select '' AS "locrefdata.dataload_schedular_audit " ;
-select * from locrefdata.dataload_schedular_audit where scheduler_end_time::DATE = current_date;
-select '' AS "locrefdata.dataload_exception_records " ;
-select * from locrefdata.dataload_exception_records  where updated_timestamp::DATE = current_date;
+#select '' AS "locrefdata.dataload_exception_record Count" ;
+#select count(*) as total_exception  from locrefdata.dataload_exception_records;
+#select '' AS "locrefdata.dataload_schedular_audit" ;
+#select * from locrefdata.dataload_schedular_audit where scheduler_end_time::DATE = current_date;
+#select '' AS "locrefdata.dataload_exception_records" ;
+#select * from locrefdata.dataload_exception_records  where updated_timestamp::DATE = current_date;
 EOF
