b7ae72ec9728af2f9734b1fd458a0febd1ef899b
mokainos <72137340+mokainos@users.noreply.github.com>
2021-11-01 17:12:18 +0000

    file compress

--- email_report.sh
+++ email_report.sh
@@ -11,7 +11,7 @@ DEFAULT_DATE=$(date +%Y%m%d)
 DAYSAGO=$(date -d "7 days ago" '+%Y%m%d 00:00:00')
 OUTPUT_DIR=/tmp
 OUTPUT_FILE_NAME=${DEFAULT_DATE}_${AZURE_DB}.csv
-ATTACHMENT={OUTPUT_DIR}/${OUTPUT_FILE_NAME}
+ATTACHMENT=${OUTPUT_DIR}/${OUTPUT_FILE_NAME}
 
 function errorHandler() {
   local dump_failed_error="${AZURE_HOSTNAME} ${AZURE_DB} Dump extract for ${DEFAULT_DATE}"
@@ -23,8 +23,6 @@ trap errorHandler ERR
 
 psql -t -U "${AZURE_DB_USERNAME}"  -h ${AZURE_HOSTNAME}  -d ${AZURE_DB} -c "$(eval "${QUERY}")"  > ${OUTPUT_DIR}/${OUTPUT_FILE_NAME}
 log "Finished dumping Report on ${DEFAULT_DATE}"
-cat ${OUTPUT_DIR}/${OUTPUT_FILE_NAME}
-cat ${ATTACHMENT}
 log "Sending email with  Report results to: ${TO_ADDRESS} ${CC_ADDRESS}"
 
 filesize=$(wc -c ${ATTACHMENT} | awk '{print $1}')
@@ -33,6 +31,7 @@ then
   gzip ${ATTACHMENT}
   ATTACHMENT=${ATTACHMENT}.gz
 fi
+echo ${ATTACHMENT}
 
 swaks -f $FROM_ADDRESS -t $TO_ADDRESS,$CC_ADDRESS --server smtp.sendgrid.net:587   --auth PLAIN -au apikey -ap $SENDGRID_APIKEY -attach ${$ATTACHMENT} --header "Subject: ${SUBJECT}" --body "Please find attached report from ${AZURE_HOSTNAME}/${AZURE_DB}"
 log "email sent"
