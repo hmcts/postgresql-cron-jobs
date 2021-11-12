e60d7b405b006b6106476b57f28a6c30bbd0296d
mokainos <72137340+mokainos@users.noreply.github.com>
2021-11-12 11:24:44 +0000

    update

--- scripts/monthly/dm-store-postgres-db-prod-evidence-monthly-documentttlcheckreport.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat <<EOF
-COPY (
-select createdbyservice, deleted, harddeleted, extract(year from ttl) || '-' || extract(month from ttl) as year_month, count ( * )
-from storeddocument
-where ttl is not null
-group by createdbyservice, deleted, harddeleted, year_month
-order by 4 desc) TO STDOUT WITH CSV HEADER ;
-EOF
