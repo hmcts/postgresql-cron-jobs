1e7e017890c16783063592e7af1b81ac5b107977
mokainos <72137340+mokainos@users.noreply.github.com>
2021-11-15 14:51:42 +0000

    exceptions

--- scripts/daily/rd-judicial-api-postgres-db-prod-dbjuddata-daily-rowcount.sh
+++ scripts/daily/rd-judicial-api-postgres-db-prod-dbjuddata-daily-rowcount.sh
@@ -1,7 +1,54 @@
 #!/bin/bash
-cat <<EOF
-select 'judicial_user_profile Count' ;
-select count (*) from dbjuddata.judicial_user_profile;
-select 'dbjuddata.judicial_office_appointment Count' ;
-select count (*) from dbjuddata.judicial_office_appointment;
-EOF
+set -fex
+
+function log() {
+  echo $(date --rfc-3339=seconds)" ${1}"
+}
+
+# Set VArs
+AZURE_HOSTNAME="rd-location-ref-api-postgres-db-prod.postgres.database.azure.com"
+AZURE_DB_USERNAME="dbrdlocationref@rd-location-ref-api-postgres-db-prod"
+AZURE_DB="dbrdlocationref"
+
+YESTERDAY=$(date -d "yesterday" '+%Y%m%d')
+DEFAULT_DATE=$(date +%Y%m%d)
+DAYSAGO=$(date -d "7 days ago" '+%Y%m%d 00:00:00')
+OUTPUT_DIR=/tmp
+FILESUB=$(echo ${SUBJECT} | cut -d' ' -f 1,2,3 | tr ' ' -)
+OUTPUT_FILE_NAME=${DEFAULT_DATE}_${AZURE_DB}_${FILESUB}.csv
+ATTACHMENT=${OUTPUT_DIR}/${OUTPUT_FILE_NAME}
+
+function errorHandler() {
+  local dump_failed_error="${AZURE_HOSTNAME} ${AZURE_DB} Dump extract for ${DEFAULT_DATE}"
+  log "${dump_failed_error}"
+  echo ""
+}
+
+trap errorHandler ERR
+echo ""  >> ${ATTACHMENT}
+echo "judicial_user_profile Count :"  >> ${ATTACHMENT}
+psql -t -U ${AZURE_DB_USERNAME} -h ${AZURE_HOSTNAME}  -d ${AZURE_DB} -c "select count (*) from dbjuddata.judicial_user_profile;"  >> ${ATTACHMENT}
+
+echo ""  >> ${ATTACHMENT}
+echo "dbjuddata.judicial_office_appointment Count"  >> ${ATTACHMENT}
+psql -t -U ${AZURE_DB_USERNAME} -h ${AZURE_HOSTNAME}  -d ${AZURE_DB} -c "select count (*) from dbjuddata.judicial_office_appointment;"  >> ${ATTACHMENT}
+
+
+echo ""  >> ${ATTACHMENT}
+
+
+
+log "Finished dumping Report on ${DEFAULT_DATE}"
+log "Sending email with  Report results to: ${TO_ADDRESS} ${CC_ADDRESS}"
+
+filesize=$(wc -c ${ATTACHMENT} | awk '{print $1}')
+if [[ $filesize -gt 1000000 ]]
+then
+  gzip ${ATTACHMENT}
+  ATTACHMENT=${ATTACHMENT}.gz
+fi
+echo ${ATTACHMENT}
+
+swaks -f $FROM_ADDRESS -t $TO_ADDRESS,$CC_ADDRESS --server smtp.sendgrid.net:587   --auth PLAIN -au apikey -ap $SENDGRID_APIKEY -attach ${ATTACHMENT} --header "Subject: ${SUBJECT}" --body "Please find attached report from ${AZURE_HOSTNAME}/${AZURE_DB}"
+log "email sent"
+rm ${ATTACHMENT}
