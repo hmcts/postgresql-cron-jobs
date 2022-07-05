185cd7da83e58c535eb0ddfeaf6b4b5f83fe057c
kunlebalogun <alliu.balogun@hackney.gov.uk>
2022-07-05 12:21:34 +0100

    Amend destination email to monthly report plus extras

--- scripts/daily/ccd-data-store-api-postgres-db-v11-prod-ccd_data_store-daily-stopsappliedtogrants.sh
+++ scripts/daily/ccd-data-store-api-postgres-db-v11-prod-ccd_data_store-daily-stopsappliedtogrants.sh
@@ -11,5 +11,4 @@ SELECT to_char(CAST (last_modified AS DATE), 'DD/MM/YYYY')  AS date_of_stop,
  WHERE jurisdiction = 'PROBATE' AND state='BOCaseStopped'
 AND data #>> '{boCaseStopReasonList}' IS NOT NULL
 AND last_modified::date = '${YESTERDAY}'  ORDER BY 3 ) to stdout with csv header;
---AND last_modified::date between '20190326' and '20190520'  ORDER BY 3 ) to stdout with csv header;
 EOF
