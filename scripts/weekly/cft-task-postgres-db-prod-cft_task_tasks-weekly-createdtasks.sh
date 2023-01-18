d15d55412f8e692683c230a9112e88929dcc3aae
Alliu Balogun <alliu.balogun@hmcts.net>
2023-01-18 10:41:33 +0000

    Change permissions on cft script

--- scripts/weekly/cft-task-postgres-db-prod-cft_task_tasks-weekly-createdtasks.sh
+++ scripts/weekly/cft-task-postgres-db-prod-cft_task_tasks-weekly-createdtasks.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 cat <<EOF
-COPY (
-SELECT t.created AS created,
-t.case_id,
-t.task_id,
-t.role_category,
-t.task_name,
-t.task_type,
-t.location_name,
-t.due_date_time,
-t.assignment_expiry,
-t.state
-FROM cft_task_db.tasks t
-where t.created >= (current_date-7 + time '00:00')
-AND t.created < (current_date + time '00:00')
-ORDER BY t.created ASC
-) TO STDOUT WITH CSV HEADER;
+COPY (SELECT t.created AS created, t.case_id,t.task_id,t.role_category,t.task_name,t.task_type,t.location_name,t.due_date_time,t.assignment_expiry,t.state FROM cft_task_db.tasks t where t.created >= (current_date-7 + time '00:00') AND t.created < (current_date + time '00:00') ORDER BY t.created ASC) TO STDOUT WITH CSV HEADER;
 EOF
