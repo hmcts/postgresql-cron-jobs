bbdd41976d26a4bb8761fcb5e90e025237168a03
Rafal Kalita <rkalitainfo@gmail.com>
2023-03-08 16:46:24 +0000

    RWA-2376 Add last_updated timestamp, user and action in weekly (#39)

--- scripts/weekly/cft-task-postgres-db-prod-cft_task_tasks-weekly-createdtasks.sh
+++ scripts/weekly/cft-task-postgres-db-prod-cft_task_tasks-weekly-createdtasks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat <<EOF
-COPY (SELECT t.created AS created, t.case_id,t.task_id,t.role_category,t.task_name,t.task_type,t.location_name,t.due_date_time,t.assignment_expiry,t.state FROM cft_task_db.tasks t where t.created >= (current_date-7 + time '00:00:00.000000') ORDER BY t.created) TO STDOUT WITH CSV HEADER;
+COPY (SELECT t.created AS created, t.case_id,t.task_id,t.role_category,t.task_name,t.task_type,t.location_name,t.due_date_time,t.assignment_expiry,t.state,t.last_updated_timestamp,t.last_updated_user,t.last_updated_action FROM cft_task_db.tasks t where t.created >= (current_date-7 + time '00:00:00.000000') ORDER BY t.created) TO STDOUT WITH CSV HEADER;
 EOF
