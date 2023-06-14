0dc18c1396262fc76ac19f1dacf7b3f7fc8e1e39
Filippo Possenti <filippo.possenti@version1.com>
2023-06-14 15:46:08 +0100

    Minor refactoring

--- scripts/weekly-report-3144/run-sort-csv-by-date.sh
+++ scripts/weekly-report-3144/run-sort-csv-by-date.sh
@@ -2,9 +2,7 @@
 
 set -x
 
-AZ_HOST=""
-DESTINATION=""
-ATTACHMENT="${DESTINATION}/${OUTPUT_FILE_NAME}"
+ATTACHMENT="${OUTPUT_FILE_NAME}"
 
 if ! [[ -e "${ATTACHMENT}" ]];
 then
@@ -12,16 +10,12 @@ then
     exit 1
 fi
 
-# Copy from vm to local directory
-echo "Copying ${OUTPUT_FILE_NAME} from vm to local"
-sudo scp -F ~/.ssh/config ${AZ_HOST}:${OUTPUT_FILE_NAME} ${DESTINATION}
-
 # Prints first 3 results
 chmod 777 ${ATTACHMENT}
 
 echo "Sorting... Displaying first three results"
 # Sorts csv file by dateUploaded
-csvsort --reverse -c 7 ${ATTACHMENT} > ${DESTINATION}/${DEFAULT_DATE}-weekly-cases-sorted.csv
+csvsort --reverse -c 7 ${ATTACHMENT} > ${DEFAULT_DATE}-weekly-cases-sorted.csv
 
 
 export ATTACHMENT
