63676adceae4903c1db9dbc6bb193aa1a896a3c7
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-08-24 12:43:06 +0100

    DTSPB-3754 fixing typo

--- scripts/daily/ccd-data-store-api-postgres-db-v11-prod-ccd_data_store-daily-stopsappliedtogrants.sh
+++ scripts/daily/ccd-data-store-api-postgres-db-v11-prod-ccd_data_store-daily-stopsappliedtogrants.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-YESTERDAY=$(date -d "yesterday" '+%Y-%m-%d')
+YESTERDAY=$(date -d "yesterday" '+%Y%m%d')
 cat <<EOF
 COPY (
 SELECT to_char(CAST (cd.data ->> 'grantStoppedDate' AS DATE), 'DD/MM/YYYY')  AS date_of_stop,
