0959ba47e1a18426473a1bffe73afb45f8ecc879
Tony Chow <36265570+tchow8@users.noreply.github.com>
2023-12-07 09:03:26 +0000

    Update pcq-backend-prod-pcq-weekly-pcqweeklydump.sh

--- scripts/weekly/pcq-backend-prod-pcq-weekly-pcqweeklydump.sh
+++ scripts/weekly/pcq-backend-prod-pcq-weekly-pcqweeklydump.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-DAYSAGO=$(date -d "8 days ago" '+%Y%m%d 00:00:00')
 cat <<EOF
 COPY (
- SELECT * FROM protected_characteristics WHERE completed_date >= '${DAYSAGO}' ORDER BY completed_date) TO STDOUT with csv header ;
+ SELECT * FROM protected_characteristics WHERE completed_date >= '20231120 00:00:00' AND completed_date < '20231204 00:00:00' ORDER BY completed_date) TO STDOUT with csv header ;
 EOF
