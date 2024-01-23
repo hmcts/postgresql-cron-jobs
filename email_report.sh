8da066d5f007b6db85d3254f28d475019d2335a9
Tony Chow <tony.chow@hmcts.net>
2024-01-23 11:46:34 +0000

    Fixed file output name

--- email_report.sh
+++ email_report.sh
@@ -11,11 +11,11 @@ DEFAULT_DATE=$(date +%Y%m%d)
 DAYSAGO=$(date -d "7 days ago" '+%Y%m%d 00:00:00')
 OUTPUT_DIR=/tmp
 FILESUB=$(echo ${SUBJECT} | cut -d' ' -f 1,2,3 | tr ' ' -)
-OUTPUT_FILE_NAME=${DEFAULT_DATE}_${AZURE_DB}_${FILESUB}.csv
+OUTPUT_FILE_NAME=${YESTERDAY}_${AZURE_DB}_${FILESUB}.csv
 ATTACHMENT=${OUTPUT_DIR}/${OUTPUT_FILE_NAME}
 
 function errorHandler() {
-  local dump_failed_error="${AZURE_HOSTNAME} ${AZURE_DB} Dump extract for ${DEFAULT_DATE}"
+  local dump_failed_error="${AZURE_HOSTNAME} ${AZURE_DB} Dump extract for ${YESTERDAY}"
   log "${dump_failed_error}"
   echo ""
 }
@@ -23,7 +23,7 @@ function errorHandler() {
 trap errorHandler ERR
 
 psql -t -U "${AZURE_DB_USERNAME}"  -h ${AZURE_HOSTNAME}  -d ${AZURE_DB} -c "$(eval "${QUERY}")"  > ${OUTPUT_DIR}/${OUTPUT_FILE_NAME}
-log "Finished dumping Report on ${DEFAULT_DATE}"
+log "Finished dumping Report on ${YESTERDAY}"
 log "Sending email with  Report results to: ${TO_ADDRESS} ${CC_ADDRESS}"
 
 
@@ -32,7 +32,7 @@ echo "${ATTACHMENT} is $filesize bytes in size"
 if [[ $filesize -gt 9000000 ]]
 then
   az login --identity
-  az storage blob upload --account-name "miapintegrationprod"  --auth-mode login  --container-name "${CONTAINER_NAME}"  --name "${OUTPUT_FILE_NAME}" --file "${ATTACHMENT}"
+  az storage blob upload --account-name "miapintegrationprod"  --auth-mode login  --container-name "${CONTAINER_NAME}"  --name "${OUTPUT_FILE_NAME}" --file "${ATTACHMENT}" --overwrite
   log "upload file to storage account"
 else
   swaks -f $FROM_ADDRESS -t $TO_ADDRESS,$CC_ADDRESS --server smtp.sendgrid.net:587   --auth PLAIN -au apikey -ap $SENDGRID_APIKEY -attach ${ATTACHMENT} --header "Subject: ${SUBJECT}" --body "Please find attached report from ${AZURE_HOSTNAME}/${AZURE_DB}"
