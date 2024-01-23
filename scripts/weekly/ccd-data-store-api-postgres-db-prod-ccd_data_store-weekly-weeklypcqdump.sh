72a5728bdc421f4974fe289970760545833e5309
Tony Chow <36265570+tchow8@users.noreply.github.com>
2024-01-23 10:55:13 +0000

    Update pcq dump to start from yesterday

--- scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-weeklypcqdump.sh
+++ scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-weeklypcqdump.sh
@@ -1,5 +1,5 @@
 cat <<EOF
-DAYSAGO=$(date -d "7 days ago" '+%Y%m%d 00:00:00')
+DAYSAGO=$(date -d "8 days ago" '+%Y%m%d 00:00:00')
 COPY (
  SELECT * FROM protected_characteristics WHERE completed_date >= '${DAYSAGO}' ORDER BY completed_date) TO STDOUT with csv header ;
 EOF
