00fee0d57a19d98884d4d161d846307b1eccdc4f
Tony Chow <36265570+tchow8@users.noreply.github.com>
2024-01-23 10:53:20 +0000

    Set PCQ to start from yesterday

--- scripts/weekly/pcq-backend-prod-pcq-weekly-pcqweeklydump.sh
+++ scripts/weekly/pcq-backend-prod-pcq-weekly-pcqweeklydump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DAYSAGO=$(date -d "8 days ago" '+%Y%m%d 00:00:00')
+DAYSAGO=$(date -d "9 days ago" '+%Y%m%d 00:00:00')
 cat <<EOF
 COPY (
  SELECT * FROM protected_characteristics WHERE completed_date >= '${DAYSAGO}' ORDER BY completed_date) TO STDOUT with csv header ;
