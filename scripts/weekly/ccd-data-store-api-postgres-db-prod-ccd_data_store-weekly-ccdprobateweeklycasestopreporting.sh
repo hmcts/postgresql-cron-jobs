3c3bc266ad175b98084dfcbb6dc412b4feadd9cf
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-03-01 13:59:53 +0000

    DTSPB-3940 Probate case stopped weekly report

---
+++ scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-ccdprobateweeklycasestopreporting.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cat <<EOF
+COPY (
+SELECT to_char(CAST (ce.data ->> 'grantStoppedDate' AS DATE), 'DD/MM/YYYY')  AS date_of_stop,
+trim(cd.data->>'registryLocation') AS registry,
+cd.reference AS case_number,
+CONCAT(cd.data ->> 'deceasedSurname', ' ',cd.data ->> 'deceasedForenames') AS full_name,
+jsonb_array_elements(ce.data -> 'boCaseStopReasonList') -> 'value' ->> 'caseStopReason' AS stop_reason,
+ce.user_first_name || ' ' || ce.user_last_name as author
+FROM case_data as cd , case_event as ce
+WHERE cd.jurisdiction = 'PROBATE' AND cd.state='BOCaseStopped'
+AND cd.id = ce.case_data_id
+AND cd.data #>> '{boCaseStopReasonList}' IS NOT NULL
+AND CAST (cd.data ->> 'grantStoppedDate' AS DATE) between (current_date-7 + time '00:00') and (current_date + time '00:00')
+AND ce.event_id in ( 'boFailQA', 'boStopCaseForCaseMatchingForExamining', 'boStopCaseForRegistrarEscalations', 'boStopCaseForCasePrinted', 'boStopCaseForCaseMatching', 'boStopCaseForCaseCreated', 'boStopCaseForGrantReissueExamining', 'boStopCaseForGrantReissueMatching' )
+ORDER BY 3,1 desc
+) TO STDOUT WITH CSV HEADER ;
+EOF
