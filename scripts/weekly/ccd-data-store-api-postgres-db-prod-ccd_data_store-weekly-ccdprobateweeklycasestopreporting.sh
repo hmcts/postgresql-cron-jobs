b36ef1165685915bd600a61cd98ed76858f6a1bf
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-03-04 12:00:11 +0000

    DTSPB-3940 Probate case stopped weekly report query

--- scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-ccdprobateweeklycasestopreporting.sh
+++ scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-ccdprobateweeklycasestopreporting.sh
@@ -2,17 +2,22 @@
 cat <<EOF
 COPY (
 SELECT to_char(CAST (ce.data ->> 'grantStoppedDate' AS DATE), 'DD/MM/YYYY')  AS date_of_stop,
-trim(cd.data->>'registryLocation') AS registry,
-cd.reference AS case_number,
-CONCAT(cd.data ->> 'deceasedSurname', ' ',cd.data ->> 'deceasedForenames') AS full_name,
+trim(ce.data->>'registryLocation') AS registry,
+case_stopped_lastweek.reference AS case_number,
+CONCAT(ce.data ->> 'deceasedSurname', ' ',ce.data ->> 'deceasedForenames') AS full_name,
 jsonb_array_elements(ce.data -> 'boCaseStopReasonList') -> 'value' ->> 'caseStopReason' AS stop_reason,
 ce.user_first_name || ' ' || ce.user_last_name as author
-FROM case_data as cd , case_event as ce
-WHERE cd.jurisdiction = 'PROBATE' AND cd.state='BOCaseStopped'
-AND cd.id = ce.case_data_id
-AND cd.data #>> '{boCaseStopReasonList}' IS NOT NULL
-AND CAST (cd.data ->> 'grantStoppedDate' AS DATE) between (current_date-7 + time '00:00') and (current_date + time '00:00')
-AND ce.event_id in ( 'boFailQA', 'boStopCaseForCaseMatchingForExamining', 'boStopCaseForRegistrarEscalations', 'boStopCaseForCasePrinted', 'boStopCaseForCaseMatching', 'boStopCaseForCaseCreated', 'boStopCaseForGrantReissueExamining', 'boStopCaseForGrantReissueMatching' )
+FROM case_event as ce, (SELECT distinct cd.id as caseID, reference
+                                FROM case_event as ce_stopped_lastweek INNER JOIN case_data cd ON cd.id = ce_stopped_lastweek.case_data_id AND cd.jurisdiction = 'PROBATE'
+                                WHERE ce_stopped_lastweek.case_type_id = 'GrantOfRepresentation'
+                                AND ce_stopped_lastweek.data #>> '{boCaseStopReasonList}' IS NOT NULL
+                                AND ce_stopped_lastweek.event_id in ('boStopCase', 'boFailQA', 'boStopCaseForCaseMatchingForExamining','boStopCaseForRegistrarEscalations', 'boStopCaseForCasePrinted','boStopCaseForCaseMatching', 'boStopCaseForCaseCreated','boStopCaseForGrantReissueExamining', 'boStopCaseForGrantReissueMatching')
+                                AND ce_stopped_lastweek.created_date between (current_date - 7 + time '00:00') and (current_date + time '00:00')
+                                ) as case_stopped_lastweek
+WHERE case_stopped_lastweek.caseID = ce.case_data_id
+AND ce.case_type_id = 'GrantOfRepresentation'
+AND ce.event_id in ('boStopCase', 'boFailQA', 'boStopCaseForCaseMatchingForExamining','boStopCaseForRegistrarEscalations', 'boStopCaseForCasePrinted','boStopCaseForCaseMatching', 'boStopCaseForCaseCreated','boStopCaseForGrantReissueExamining', 'boStopCaseForGrantReissueMatching')
+AND ce.data #>> '{boCaseStopReasonList}' IS NOT NULL
 ORDER BY 3,1
 ) TO STDOUT WITH CSV HEADER ;
 EOF
