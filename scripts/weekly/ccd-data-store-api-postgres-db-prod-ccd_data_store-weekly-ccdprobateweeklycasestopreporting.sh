a4becb7b91397de004006e225224a3ef9139d00f
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-04-11 14:04:28 +0100

    DTSPB-3940 Probate case stopped weekly report query

--- scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-ccdprobateweeklycasestopreporting.sh
+++ scripts/weekly/ccd-data-store-api-postgres-db-prod-ccd_data_store-weekly-ccdprobateweeklycasestopreporting.sh
@@ -11,13 +11,13 @@ FROM case_event as ce, (SELECT distinct cd.id as caseID, reference
                                 FROM case_event as ce_stopped_lastweek INNER JOIN case_data cd ON cd.id = ce_stopped_lastweek.case_data_id AND cd.jurisdiction = 'PROBATE'
                                 WHERE ce_stopped_lastweek.case_type_id = 'GrantOfRepresentation'
                                 AND ce_stopped_lastweek.data #>> '{boCaseStopReasonList}' IS NOT NULL
-                                AND ce_stopped_lastweek.event_id in ('boStopCase', 'boFailQA', 'boStopCaseForCaseMatchingForExamining','boStopCaseForRegistrarEscalations', 'boStopCaseForCasePrinted','boStopCaseForCaseMatching', 'boStopCaseForCaseCreated','boStopCaseForGrantReissueExamining', 'boStopCaseForGrantReissueMatching')
+                                AND ce_stopped_lastweek.event_id in ('boStopCase', 'boStopCaseForCaseMatchingForExamining','boStopCaseForRegistrarEscalations', 'boStopCaseForCasePrinted','boStopCaseForCaseMatching', 'boStopCaseForCaseCreated','boStopCaseForGrantReissueExamining', 'boStopCaseForGrantReissueMatching')
                                 AND ce_stopped_lastweek.created_date between (current_date - 7 + time '00:00') and (current_date + time '00:00')
                                 ) as case_stopped_lastweek
 WHERE case_stopped_lastweek.caseID = ce.case_data_id
 AND ce.case_type_id = 'GrantOfRepresentation'
 AND ce.event_id in ('boStopCase', 'boFailQA', 'boStopCaseForCaseMatchingForExamining','boStopCaseForRegistrarEscalations', 'boStopCaseForCasePrinted','boStopCaseForCaseMatching', 'boStopCaseForCaseCreated','boStopCaseForGrantReissueExamining', 'boStopCaseForGrantReissueMatching')
 AND ce.data #>> '{boCaseStopReasonList}' IS NOT NULL
-ORDER BY 3,1
+ORDER BY 3,CAST (ce.data ->> 'grantStoppedDate' AS DATE)
 ) TO STDOUT WITH CSV HEADER ;
 EOF
