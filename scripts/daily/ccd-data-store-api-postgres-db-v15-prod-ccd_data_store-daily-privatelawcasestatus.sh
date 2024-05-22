1248344489566377cac62636ee1b7cce9c1a03f3
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-05-22 13:55:39 +0100

    Save file

--- scripts/daily/ccd-data-store-api-postgres-db-v15-prod-ccd_data_store-daily-privatelawcasestatus.sh
+++ scripts/daily/ccd-data-store-api-postgres-db-v15-prod-ccd_data_store-daily-privatelawcasestatus.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cat <<EOF
 COPY (
-SELECT cd.reference, ce.created_date AS event_date,cd.last_state_modified_date, ce.state_id, ce.event_name, cd.data->>'D8DivorceUnit' AS D8DivorceUnit, cd.data->'D8PetitionerHomeAddress'->>'PostCode' AS D8PetitionerHomeAddress_postcode, ce.user_first_name,ce.user_last_name, cd.data->>'LanguagePreferenceWelsh' AS Welsh_Flag  FROM case_data cd JOIN case_event ce ON cd.id = ce.case_data_id WHERE cd.case_type_id='DIVORCE' AND upper(cd.data->>'LanguagePreferenceWelsh')='YES' ORDER BY 1,2 desc) TO STDOUT with csv header ;
+SELECT reference as "Case number",  data -> 'courtName' as "Court name", data -> 'caseTypeOfApplication' as "Type of application",
+data->'familymanCaseNumber' as "Family man ID", data -> 'dateSubmitted' as "Date submitted",
+data->'caseStatus'-> 'state' as "State"
+from case_data where case_type_id='PRLAPPS' and jurisdiction='PRIVATELAW'
+and TO_DATE(data ->> 'dateSubmitted','YYYY-MM-DD') = CURRENT_DATE order by data -> 'dateSubmitted' desc;
 EOF
