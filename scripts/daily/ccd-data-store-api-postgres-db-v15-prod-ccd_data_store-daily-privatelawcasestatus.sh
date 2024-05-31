072961347febd8a8cc71e33645738b1eff775bfd
Shashi Kumar <66266334+shashisk97@users.noreply.github.com>
2024-05-31 12:47:37 +0100

    PRL daily extract job changes

--- scripts/daily/ccd-data-store-api-postgres-db-v15-prod-ccd_data_store-daily-privatelawcasestatus.sh
+++ scripts/daily/ccd-data-store-api-postgres-db-v15-prod-ccd_data_store-daily-privatelawcasestatus.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cat <<EOF
 COPY (
-SELECT reference as "Case number",  data -> 'courtName' as "Court name", data -> 'caseTypeOfApplication' as "Type of application",
+SELECT '"'||reference ||'"' as "Case number",  data -> 'courtName' as "Court name", data -> 'caseTypeOfApplication' as "Type of application",
 data->'familymanCaseNumber' as "Family man ID", data -> 'dateSubmitted' as "Date submitted",
 data->'caseStatus'-> 'state' as "State"
-from case_data where case_type_id='PRLAPPS' and jurisdiction='PRIVATELAW'
-and TO_DATE(data ->> 'dateSubmitted','YYYY-MM-DD') = CURRENT_DATE order by data -> 'dateSubmitted' desc ) to stdout with csv header;
+from case_data where case_type_id='PRLAPPS' and jurisdiction='PRIVATELAW' and data ->> 'dateSubmitted' >= '2024-04-10' order by data -> 'dateSubmitted' desc ) to stdout with csv header;
 EOF
