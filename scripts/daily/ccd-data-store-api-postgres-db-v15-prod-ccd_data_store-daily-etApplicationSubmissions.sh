4aabec85b26308acaf06c32915ed45a9e4ada2c6
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-06-06 14:57:43 +0100

    Try and fix blank files?

--- scripts/daily/ccd-data-store-api-postgres-db-v15-prod-ccd_data_store-daily-etApplicationSubmissions.sh
+++ scripts/daily/ccd-data-store-api-postgres-db-v15-prod-ccd_data_store-daily-etApplicationSubmissions.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 cat <<EOF
 COPY (
-select data ->> 'managingOffice'     as Office,
-       reference::text,
-       data ->> 'ethosCaseReference' as ethosRef,
-       application.application_type,
-       application.application_date,
-       application.applicant
+select data ->> 'managingOffice' as Office,
+reference::text,
+data ->> 'ethosCaseReference' as ethosRef,
+application.application_type,
+application.application_date,
+application.applicant
 from case_data
-         cross join lateral (
-    select applications -> 'value' ->> 'type'                           as application_type,
-           TO_DATE(applications -> 'value' ->> 'date', 'DD Month YYYY') as application_date,
-           applications -> 'value' ->> 'applicant'                      as applicant
-    from jsonb_array_elements(case_data.data -> 'genericTseApplicationCollection') as applications
-    ) as application
+cross join lateral (
+select applications -> 'value' ->> 'type' as application_type,
+TO_DATE(applications -> 'value' ->> 'date', 'DD Month YYYY') as application_date,
+applications -> 'value' ->> 'applicant' as applicant
+from jsonb_array_elements(case_data.data -> 'genericTseApplicationCollection') as applications
+) as application
 where case_type_id in ('ET_EnglandWales', 'ET_Scotland')
-  and data -> 'genericTseApplicationCollection' is not null
+and data -> 'genericTseApplicationCollection' is not null
 order by application.application_date desc) to stdout with csv header;
 EOF
--- scripts/daily/ccd-data-store-api-postgres-db-v15-prod-ccd_data_store-daily-etOnlineSubmissions.sh
+++ scripts/daily/ccd-data-store-api-postgres-db-v15-prod-ccd_data_store-daily-etOnlineSubmissions.sh
@@ -2,18 +2,18 @@
 cat <<EOF
 COPY (
 select data->>'managingOffice' as managingOffice,
-       case
-           when case_event.event_id = 'SUBMIT_CASE_DRAFT' then 'ET1 Online'
-           when case_event.event_id = 'submitEt1Draft' then 'MyHMCTS ET1'
-           when case_event.event_id = 'submitEt3' then 'MyHMCTS ET3'
-           else event_id
-       end as submissionSource,
-       count(*) filter (where created_date > now() - interval '24 hours') as last_24_hours,
-       count(*) filter (where created_date > now() - interval '1 week') as last_week,
-       count(*) filter (where created_date > now() - interval '1 month') as last_month,
-       count(*) as totalSubmissions
+case
+when case_event.event_id = 'SUBMIT_CASE_DRAFT' then 'ET1 Online'
+when case_event.event_id = 'submitEt1Draft' then 'MyHMCTS ET1'
+when case_event.event_id = 'submitEt3' then 'MyHMCTS ET3'
+else event_id
+end as submissionSource,
+count(*) filter (where created_date > now() - interval '24 hours') as last_24_hours,
+count(*) filter (where created_date > now() - interval '1 week') as last_week,
+count(*) filter (where created_date > now() - interval '1 month') as last_month,
+count(*) as totalSubmissions
 from case_event
 where event_id in ('submitEt1Draft', 'SUBMIT_CASE_DRAFT', 'submitEt3')
-  and case_event.case_type_id in ('ET_EnglandWales', 'ET_Scotland')
+and case_event.case_type_id in ('ET_EnglandWales', 'ET_Scotland')
 group by event_id, submissionSource, managingOffice) to stdout with csv header;
 EOF
