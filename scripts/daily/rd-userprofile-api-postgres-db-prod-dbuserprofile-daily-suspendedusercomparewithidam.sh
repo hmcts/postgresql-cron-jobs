6e39b732ee8c398921d969df7b8ba8ad728a41f3
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-06-27 10:35:55 +0100

    adding a cron job to check for suspended users

--- scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam.sh
+++ scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam.sh
@@ -9,16 +9,20 @@ function log() {
 AZURE_DB_USERNAME='pgadmin'
 AZURE_HOSTNAME='rd-user-profile-api-postgres-db-v16-aat.postgres.database.azure.com'
 AZURE_DB='dbuserprofile'
-SUBJECT='LOCATION-REF-DATA-DB Daily Report'
+
+SUBJECT='USER_PROFILE_DATA-DB Daily Report'
 TO_ADDRESS='sabina.sharangdhar@hmcts.net'
 CC_ADDRESS='sabina.sharangdhar@hmcts.net'
-YESTERDAY=$(date -d "yesterday" '+%Y%m%d')
-DEFAULT_DATE=$(date +%Y%m%d)
-DAYSAGO=$(date -d "7 days ago" '+%Y%m%d 00:00:00')
-OUTPUT_DIR=/tmp
 FILESUB=$(echo ${SUBJECT} | cut -d' ' -f 1,2,3 | tr ' ' -)
-# OUTPUT_FILE_NAME=${DEFAULT_DATE}_${AZURE_DB}_${FILESUB}.csv
+
+OUTPUT_DIR=/tmp
+OUTPUT_FILE_NAME=${DEFAULT_DATE}_${AZURE_DB}_${FILESUB}.csv
 USERPROFILEIDS=${OUTPUT_DIR}/"USERPROFILEIDS"
+
+USER="up.demo.cgi@hmcts.net"
+PASS="Password123"
+CLIENT="rd-professional-api"
+SECRET="a20c3cf7-1fb4-4bcf-89ec-963c05a13f71"
 
 function errorHandler() {
   local dump_failed_error="${AZURE_HOSTNAME} ${AZURE_DB} Dump extract for ${DEFAULT_DATE}"
@@ -29,28 +33,26 @@ function errorHandler() {
 trap errorHandler ERR
 
 echo " =====  Call IDAM to check user status ===== "
+# SSH commands to connect to the servers
+SSH_CMD1="ssh -F /Users/sabinasharangdhar/.ssh/az_config bastion-nonprod.platform.hmcts.net"
 
-# Set the hostnames database user and passwordnames for the userprofile db
-if [ $ALL_USERS_FLAG -ne  'true') ]
+if [ $ALL_USERS_FLAG -ne 0 ]
 then
-psql -t -U "${AZURE_DB_USERNAME}" -h ${AZURE_HOSTNAME}  -d ${AZURE_DB} -c "SELECT idam_id FROM dbuserprofile.user_profile where idam_status ='SUSPENDED' and last_updated >= NOW() - INTERVAL '14 DAYS' LIMIT 5;"  >> ${USERPROFILEIDS}
-else
-psql -t -U "${AZURE_DB_USERNAME}" -h ${AZURE_HOSTNAME}  -d ${AZURE_DB} -c "SELECT idam_id FROM dbuserprofile.user_profile where idam_status ='SUSPENDED' LIMIT 5;"  >> ${USERPROFILEIDS}
+PSQL_CMD1='psql "sslmode=require user='$USER1' password='$PASS1' host='$DB1_HOST' dbname='$DB1_NAME' port=5432" -c "select idam_id from user_profile where idam_status ='SUSPENDED' and last_updated >= NOW() - INTERVAL '14 DAYS' LIMIT 5 ;"'
+ else
+PSQL_CMD1='psql "sslmode=require user='$USER1' password='$PASS1' host='$DB1_HOST' dbname='$DB1_NAME' port=5432" -c "SELECT idam_id FROM dbuserprofile.user_profile u LIMIT 5;"'
 fi
-#CMD1="$SSH_CMD1 $PSQL_CMD1"
+CMD1="$SSH_CMD1 $PSQL_CMD1"
 echo "idams from user profile"
-${USERPROFILEIDS} | tail -n +3 | tee tmp/user_profile_idams.txt
+${CMD1} | tail -n +3 | tee user_profile_idams.txt
 
 while read -r line; do
   tables+=("$line")
 done < user_profile_idams.txt
 
-HEADERS='Content-Length:0,Host:idam-api.aat.platform.hmcts.net,Accept: */*,Accept-Encoding: gzip, deflate, br,Connection: keep-alive,Content-Type: application/x-www-form-urlencoded'
-
-TOKEN_CMD=$ CURL -X GET 'https://idam-api.aat.platform.hmcts.net/o/token?grant_type=password&username='+$USER+'&password='+$PASS+'&client_id='+$CLIENT+'&client_secret='+$SECRET+'&scope=openid profile roles manage-user create-user search-user', 'headers=' +$HEADERS
-result=`$TOKEN_CMD | grep "Test:"`
+HEADERS='-H Content-Length:0 -H Host:idam-api.aat.platform.hmcts.net -H Accept:*/* -H Accept-Encoding:gzip,deflate,br -H Connection:keep-alive -H Content-Type:application/x-www-form-urlencoded'
+TOKEN_CMD=$ CURL -X  GET 'https://idam-api.aat.platform.hmcts.net/o/token?grant_type=password&username='$USER'&password='$PASS'&client_id='$CLIENT'&scope=openid/profile/roles/manage-user/create-user/search-user&client_secret='$SECRET $HEADERS
 echo "TOKEN :"$TOKEN_CMD
-
 
 tables=()
 for table in ${tables[@]}; do
