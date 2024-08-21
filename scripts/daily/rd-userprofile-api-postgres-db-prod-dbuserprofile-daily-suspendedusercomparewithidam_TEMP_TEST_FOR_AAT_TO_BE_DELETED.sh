7cf6fdba748e32c04aec13a06e830db2cc9f1224
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-08-21 10:08:07 +0100

    fixing issues related to creatign tokens

--- scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam_TEMP_TEST_FOR_AAT_TO_BE_DELETED.sh
+++ scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam_TEMP_TEST_FOR_AAT_TO_BE_DELETED.sh
@@ -42,7 +42,7 @@ trap errorHandler ERR
 echo " =====  Call User Profile table and select suspended users ===== "
 
 echo "ALL_USERS_FLAG $ALL_USERS_FLAG"
-echo  $USERNAME
+echo  ${USERNAME}
 echo  $PGPASSWORD
 
 
@@ -64,8 +64,8 @@ done < SUSPENDED_USERS.txt
 
 echo  "HELLO"
 echo  $ATTACHMENT
-echo  $idam-rd-system-user-password
-
+echo  ${idam-rd-system-user-password}
+echo  ${idam-rd-system-user-password}
 # for each suspended user from user profile make a call to idam to check if the user exists
 echo -e "IDAM IDS                                                                               " "  :   " "STATUS ON IDAM" >> ${ATTACHMENT}
 echo -e "  " "      " "  " >> ${ATTACHMENT}
@@ -73,7 +73,7 @@ echo -e "  " "      " "  " >> ${ATTACHMENT}
 # generating Bearer token to connect to idam
 
 #TOKEN_CMD=$(curl -X POST 'https://idam-api.aat.platform.hmcts.net/o/token?grant_type=password&username='$USERNAME'&password='$SYSPASS'&client_secret='$OAUTH2_CLIENT_SECRET'&scope=openid%20profile%20roles%20manage-user%20create-user%20search-user&client_id=rd-professional-api'  -H 'accept: */*'  -H Connection:keep-alive -H Content-Type:application/x-www-form-urlencoded)
-TOKEN_CMD=$(curl -X POST 'https://idam-api.aat.platform.hmcts.net/o/token?grant_type=password&username=admin.refdata@hmcts.net&password='$idam-rd-system-user-password'&client_secret=a20c3cf7-1fb4-4bcf-89ec-963c05a13f71&scope=openid%20profile%20roles%20manage-user%20create-user%20search-user&client_id=rd-professional-api'  -H 'accept: */*'  -H Connection:keep-alive -H Content-Type:application/x-www-form-urlencoded)
+TOKEN_CMD=$(curl -X POST 'https://idam-api.aat.platform.hmcts.net/o/token?grant_type=password&username=admin.refdata@hmcts.net&password='${idam-rd-system-user-password}'&client_secret=a20c3cf7-1fb4-4bcf-89ec-963c05a13f71&scope=openid%20profile%20roles%20manage-user%20create-user%20search-user&client_id=rd-professional-api'  -H 'accept: */*'  -H Connection:keep-alive -H Content-Type:application/x-www-form-urlencoded)
 TOKEN=$(echo ${TOKEN_CMD} | cut -d':' -f 2 | cut -d',' -f 1 | tr -d '"' )
 
 
