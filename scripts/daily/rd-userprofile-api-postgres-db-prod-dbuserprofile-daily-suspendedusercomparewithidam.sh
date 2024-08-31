5c54b2aa568773b5e1059fee902a554ee522c939
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-08-31 09:31:22 +0100

    fixing issues related to creatign tokens

--- scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam.sh
+++ scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam.sh
@@ -11,7 +11,7 @@ function log() {
 }
 
 # Set VArs
-AZURE_DB_USERNAME='DTS JIT Access rd DB Reader SC'
+AZURE_DB_USERNAME='DTS Platform Operations SC'
 AZURE_HOSTNAME='rd-user-profile-api-postgres-db-v16-prod.postgres.database.azure.com'
 AZURE_DB='dbuserprofile'
 ALL_USERS_FLAG=${ALL_USERS_FLAG}
