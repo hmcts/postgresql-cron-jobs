25daeb45e515ab711daeddac031c5fbb9fe479d6
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-08-31 10:05:52 +0100

    fixing issues related to creating tokens

--- scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam.sh
+++ scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam.sh
@@ -55,7 +55,7 @@ while read -r line; do
 done < SUSPENDED_USERS.txt
 
 # generating Bearer token to connect to idam
-TOKEN_CMD=$(curl -X POST https://idam-api.platform.hmcts.net/o/token?grant_type=password&username=admin.refdata@hmcts.net&password=$SYSPASS&client_secret=$OAUTH2_CLIENT_SECRET&scope=openid%20profile%20roles%20manage-user%20create-user%20search-user&client_id=rd-professional-api -H Content-Length:0 -H Host:idam-api.platform.hmcts.net -H accept: */* -H Accept-Encoding:gzip,deflate, -H Connection:keep-alive -H Content-Type:application/x-www-form-urlencoded)
+TOKEN_CMD=$(curl -X POST https://idam-api.platform.hmcts.net/o/token?grant_type=password&username=admin.refdata@hmcts.net&password=$SYSPASS&client_secret=$OAUTH2_CLIENT_SECRET&scope=openid%20profile%20roles%20manage-user%20create-user%20search-user&client_id=rd-professional-api -H Content-Length:0 -H Host:idam-api.platform.hmcts.net -H accept: */* -H Accept-Encoding:gzip,deflate -H Connection:keep-alive -H Content-Type:application/x-www-form-urlencoded)
 TOKEN=$(echo ${TOKEN_CMD} | cut -d':' -f 2 | cut -d',' -f 1 | tr -d '"' )
 
 
