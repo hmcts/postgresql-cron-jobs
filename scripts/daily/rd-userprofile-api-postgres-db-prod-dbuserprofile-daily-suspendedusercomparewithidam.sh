dafd916eec4e7ffe5b3eb51b01d5ea3155643241
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-09-02 08:59:56 +0100

    fixing issues related to creating tokens

--- scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam.sh
+++ scripts/daily/rd-userprofile-api-postgres-db-prod-dbuserprofile-daily-suspendedusercomparewithidam.sh
@@ -41,21 +41,21 @@ trap errorHandler ERR
 echo " =====  Call User Profile table and select suspended users ===== "
 echo "ALL_USERS_FLAG $ALL_USERS_FLAG"
 # pick suspended users from user profile in the last 2 weeks and write them to a file
-if [ $ALL_USERS_FLAG -ne 0 ]
-then
-psql -t -U "${AZURE_DB_USERNAME}" -h ${AZURE_HOSTNAME}  -d ${AZURE_DB} -c "SELECT idam_id FROM dbuserprofile.user_profile u where idam_status ='SUSPENDED' and last_updated >= NOW() - INTERVAL '14 DAYS';" >> ${USERIDAMS}
- else
-psql -t -U "${AZURE_DB_USERNAME}" -h ${AZURE_HOSTNAME}  -d ${AZURE_DB}  -c "SELECT idam_id FROM dbuserprofile.user_profile u where idam_status ='SUSPENDED';" >> ${USERIDAMS}
-fi
+#if [ $ALL_USERS_FLAG -ne 0 ]
+#then
+#psql -t -U "${AZURE_DB_USERNAME}" -h ${AZURE_HOSTNAME}  -d ${AZURE_DB} -c "SELECT idam_id FROM dbuserprofile.user_profile u where idam_status ='SUSPENDED' and last_updated >= NOW() - INTERVAL '14 DAYS';" >> ${USERIDAMS}
+ #else
+#psql -t -U "${AZURE_DB_USERNAME}" -h ${AZURE_HOSTNAME}  -d ${AZURE_DB}  -c "SELECT idam_id FROM dbuserprofile.user_profile u where idam_status ='SUSPENDED';" >> ${USERIDAMS}
+#fi
 
 # iterate file of suspended users
-tables=()
-while read -r line; do
-  tables+=("$line")
-done < SUSPENDED_USERS.txt
-rm ${USERIDAMS}
+#tables=()
+#while read -r line; do
+  #tables+=("$line")
+#done < SUSPENDED_USERS.txt
+
 # generating Bearer token to connect to idam
-TOKEN_CMD=$(curl -X POST 'https://idam-api.platform.hmcts.net/o/token?grant_type=password&username=admin.refdata@hmcts.net&password='${SYSPASS}'&client_secret='${OAUTH2_CLIENT_SECRET}'&scope=openid%20profile%20roles%20manage-user%20create-user%20search-user&client_id=rd-professional-api' -H 'Content-Length:0' -H 'Host:idam-api.platform.hmcts.net' -H 'accept: */*' -H 'Accept-Encoding:gzip,deflate' -H 'Connection:keep-alive' -H 'Content-Type:application/x-www-form-urlencoded')
+TOKEN_CMD=$(curl -X POST 'https://idam-api.platform.hmcts.net/o/token?grant_type=password&username=admin.refdata@hmcts.net&password='${SYSPASS}'&client_secret='${OAUTH2_CLIENT_SECRET}'&scope=openid%20profile%20roles%20manage-user%20create-user%20search-user&client_id=rd-professional-api' -H 'accept: */*' -H Content-Length:0 -H Host:idam-api.platform.hmcts.net  -H Accept-Encoding:gzip,deflate -H Connection:keep-alive -H Content-Type:application/x-www-form-urlencoded)
 TOKEN=$(echo ${TOKEN_CMD} | cut -d':' -f 2 | cut -d',' -f 1 | tr -d '"' )
 
 # for each suspended user from user profile make a call to idam to check if the user exists
